b0821bead97b1df0f1d9e8f3879dd5c4b7ea05c7
root <root@localhost.localdomain>
2019-11-29 11:13:32 +0530

    Structure of ddos

---
+++ DDoS/attack.py
@@ -0,0 +1,5 @@
+# Attack code #
+# Multiple set of threads for each protocol #
+
+if __name__=="__main__":
+    print("main")
---
+++ DDoS/config.py
@@ -0,0 +1,20 @@
+# Config file for all attacks #
+
+source_ip = None
+dest_ip = None
+
+udp_on = True
+tcp_on = True
+ip_on = True
+
+udp_port = 1
+tcp_port = 1
+ip_port = 1
+
+no_udp_packets = 100
+no_tcp_packets = 100
+no_ip_packets = 100
+
+no_udp_iterations = 100
+no_tcp_iterations = 100
+no_ip_iterations = 100
