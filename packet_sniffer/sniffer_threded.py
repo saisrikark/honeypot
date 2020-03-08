c6a1b597820327e6de14b3e14fc6218b706d3e19
ShashankBongale <shashankbongale@gmail.com>
2020-03-08 11:16:33 +0530

    sniffer with threading done(except start and stop)

---
+++ packet_sniffer/sniffer_threded.py
@@ -0,0 +1,44 @@
+import pyshark
+import threading 
+import time
+
+capture = pyshark.LiveCapture(interface = 'wlp19s0')
+j = 0
+
+def write_into_file(capture_list,start_index,end_index):
+    for i in range(start_index,end_index):
+        try:
+            global j 
+            packet_log = str(j) + " " + str(capture_list[i].sniff_timestamp) + " "
+            j = j + 1
+            packet_log = packet_log + capture_list[i]['eth'].src + " " + capture_list[i]['eth'].dst + " " 
+            packet_log = packet_log + capture_list[i][capture_list[i].transport_layer.lower()].srcport + " " + capture_list[i][capture_list[i].transport_layer.lower()].dstport + " "
+            if("ip" in capture_list[i]):
+                packet_log = packet_log + capture_list[i]['ip'].src + " " + capture_list[i]['ip'].dst + "\n"
+            else:
+                packet_log = packet_log + capture_list[i]['ipv6'].src + " " + capture_list[i]['ipv6'].dst + "\n"
+            fp = open("packets.log", "a")
+            fp.write(packet_log)
+            fp.close()
+        except:
+            fp = open("packets.log","a")
+            fp.write("Failed To Log The Packet")
+            fp.close()
+
+def read_packets():
+    while(len(capture) == 0):
+        time.sleep(5)
+    older_count = 0
+    while(True):
+        new_count = len(capture)
+        if(new_count != older_count):
+            print(str(older_count) + " " + str(new_count))
+            write_into_file(capture,older_count,new_count)
+            older_count = new_count
+            time.sleep(10)
+
+t1 = threading.Thread(target=read_packets)
+t1.start()
+capture.sniff()
+
+
